16584f26bee0d64298a529a10cd6f61d3489ea8c
Magnus Quist <rydalund@outlook.com>
2025-11-28 22:04:02 +0100

    chore: disable ActiveRecord and remove db tasks from CI

--- config/application.rb
+++ config/application.rb
@@ -1,27 +1,21 @@
 require_relative "boot"
 
-require "rails/all"
+require "rails"
+require "action_controller/railtie"
+require "action_view/railtie"
+# require "active_record/railtie"
+require "active_job/railtie"
+require "action_mailer/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"  # valfritt
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module SocialSecurityFrontend
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.1
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
